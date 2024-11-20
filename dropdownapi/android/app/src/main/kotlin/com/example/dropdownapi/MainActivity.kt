fe8cf15891693b6c86d9ea62e23757ffbcf7b126
piannn01 <vian.oktavianto03@gmail.com>
2024-11-20 13:16:32 +0700

    update/revisi

--- dropdownapi/android/app/src/main/kotlin/com/example/dropdownapi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dropdownapi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- httprequest/android/app/src/main/kotlin/com/example/httprequest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.httprequest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
